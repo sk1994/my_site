e74a0b9f563ddd9e703c5a3f30cad0d550c53a11
sk1994 <satoru.kobayashi.1994.jpn@gmail.com>
2017-03-08 22:23:45 +0900

    background

--- js/part_load.js
+++ js/part_load.js
@@ -1,5 +1,5 @@
 $(function () {
-            $("#header").load("./header.html");
-           
-            $("#footer").load("./footer.html");
-        });
+    $("#header").load("./header.html");
+    $("#nav").load("./nav.html");
+    $("#footer").load("./footer.html");
+});
